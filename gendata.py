4c40152d22366d215f703c4dc03e128c1a94d859
Elin Hagman <eliyetres@hotmail.com>
2019-03-10 01:03:56 +0100

    option split train and test data

--- gendata.py
+++ gendata.py
@@ -12,17 +12,17 @@ from sklearn.linear_model import LogisticRegression
 # add whatever additional imports you may need here. You may not use the
 # scikit-learn OneHotEncoder, or any related automatic one-hot encoders.
 import re
-from nltk import ngrams
 import random
+from nltk import ngrams
+
+# python gendata.py -N 4 -S 500 -E 900 -T 100 brown_rga.txt output
 
 
-def readfile(filename, split_at, startline=0, endline=None):
+def readfile(filename, testline, startline=0, endline=None):
     """ Opens and reads a file with words and their POS-tags.
     Returns a list of words, starting and ending at n selected lines."""
-    text_tag = []
-    only_words = []
+    word_lines = []
     # Part for words only
-    wordlist = []
     with open(filename) as fp:
         for line in fp:
             w = re.sub(r'\n', '', line)
@@ -30,81 +30,65 @@ def readfile(filename, split_at, startline=0, endline=None):
             w = re.sub(r'((?<=([a-z]\'))\/([A-Z]+))', '', w)
             w = re.sub(r'((?<=[.,\!?-\`])\/([.,\!?-\`]))', '', w)
             w = re.sub(r' {1,}', ' ', w).split(" ")
-            only_words.append(w)
+            word_lines.append(w)
             # print(line)
             # print(w)
-    #endline = len(wordlist)
+    #print("Startline {}, endline {}".format(startline, endline))
     if startline > 0 and endline is not None:
-        
-        only_words = only_words[startline:endline]
-        test_selection = random.sample(range(startline, endline), args.test)
-        for lst in only_words:
-            for word in lst:
-                wordlist.append(word)
-            # Test and train data
-            test_data = wordlist[test_selection]
-            train_data = np.delete(wordlist, test_selection) 
-            return wordlist
-    
-    if startline > 0:
-        only_words = only_words[startline:]
-        
-    if endline is not None:
-        only_words = only_words[endline:]
-        
-    # ((?<=[a-z])\/([A-Z]+-?)+)|((?<=[.,\'])\/[.,\'])
-    # This part is for the words with tags
-    #taglist = text.split(" ")
-    """ for pair in taglist:
-    p = tuple(pair.split("/"))        
-    text_tag.append(p) """
+        word_lines = word_lines[startline:endline]
+    elif startline > 0 and endline is None:
+        word_lines = word_lines[startline:]
+    elif startline == 0 and endline is not None:
+        word_lines = word_lines[endline:]
 
-    for lst in only_words:
-        for word in lst:
-            wordlist.append(word)
-    # Test and train data
-    test_selection = random.sample(wordlist, args.test)
-    test_data = wordlist[test_selection]
-    train_data = np.delete(wordlist, test_selection)
+    tr = word_lines[testline:]  # Split training and test lines
+    te = word_lines[:testline]
+    print("Train data lines: ", len(tr))
+    print("Test data lines: ", len(te))
+    train_data = [item for line in tr for item in line]  # Flatten lists
+    test_data = [item for line in te for item in line]
 
-    print("Train data, ", train_data)
-    print("Test data :", test_data)
-    return wordlist
+    return train_data, test_data
 
-readfile("brown_rga.txt", 10)
 
-def generate_vocab(wordlist):
-    """ Takes a list of all  words in the vocabulary.
+def generate_vocab(data_list):
+    """ Takes a list of the separated training and test data words in the vocabulary.
     Returns an enumerated list of tuples for each word and its corresponding ID """
-    vocab = list(enumerate(set(wordlist+["<s>"]))
+    vocab = list(enumerate(set(data_list+["<s>"]))
                  )  # Adding start tag to end of vocabulary
-    print("Example of word and it's ID: ", vocab[:2], vocab[:-2])
+    print("Example of word and it's ID: ", vocab[0], vocab[-1])
+
     return vocab
 
 
 def create_ngram(words, n=3):
     """ Takes a list of word form the vocabulary.
     Returns the selected number of n-grams as a list of tuples, default is 3. """
-    n_grams = ngrams(words, n, pad_left=True,
-                     left_pad_symbol='<s>', pad_right=False)  # Padding n-grams with start tag
-    return n_grams
+    grams = ngrams(words, n, pad_left=True,
+                   left_pad_symbol='<s>', pad_right=False)  # Padding n-grams with start tag
+
+    return grams
 
 
 def one_hot(vocab, n_grams):
     """ Takes a numpy array of n-grams and the full vocabulary.
     Matches the words' ID with the words in the n-gram, one-hot encodes them and adds the ending word of the n-gram as a label.
     Returns a numpy array of the n-grams' one-hot encodings and labels. """
-
     ngram_len = len(n_grams)
     print("Number of words in the vocabulary: ", len(vocab))
     print("Number of n-grams: ", ngram_len)
-    vocab_ids = [id[0] for id in vocab]  # Word ids    
-    arr = np.eye(ngram_len, dtype=int)[vocab_ids] # Fill an array with 0s, add 1
+    vocab_ids = [id[0] for id in vocab]  # Word ids
+    print("Generating ids", vocab_ids)
+    # Fill an array with 0s, add 1
+    arr = np.eye(ngram_len, dtype=int)[vocab_ids]
+    print("Creating arrays: ", arr)
     v_len = ngram_len * (args.ngram-1)  # Fill an empty array
     one_hot_vectors = np.empty((0, v_len), int)
+    print("Creating empty vectors")
     class_value_labels = []
     for gram in n_grams:
         # Use last word of n-gram as class value label
+        print("Appending n-gram")
         class_value_labels.append(gram[-1])
         ngram_vector = np.array([], dtype=int)
         for word in gram[:-1]:
@@ -112,7 +96,7 @@ def one_hot(vocab, n_grams):
             word_arr = arr[word_index[0][0]]  # Corresponding index
             ngram_vector = np.append(ngram_vector, word_arr)
         one_hot_vectors = np.vstack([one_hot_vectors, ngram_vector])
-    print("Concat vector: ", one_hot_vectors)
+    #print("Concat vector: ", one_hot_vectors)
     vector_obj = pd.DataFrame(data=one_hot_vectors)
     vector_obj['label'] = class_value_labels  # Add word labels to last column
 
@@ -120,12 +104,8 @@ def one_hot(vocab, n_grams):
 
 
 def print_to_file(data, filename):
-    """Takes a data object and prints it to a CVS file. """
-    if filename[-3:] == "csv":
-        print("Creating csv file.")
-        pd.DataFrame(data).to_csv(filename, mode='w')
-    else:
-        np.savetxt(filename, data) 
+    """Takes a data object, a name label (test or train) and prints it to a CVS file. """
+    pd.DataFrame(data).to_csv(filename, mode='w')
 
 
 parser = argparse.ArgumentParser(description="Convert text to features")
@@ -147,28 +127,43 @@ parser.add_argument("outputfile", type=str,
 
 args = parser.parse_args()
 
+if args.test > (args.endline - args.startline):
+    exit("Error: Test data sample must be smaller than selected lines sample.")
+if args.startline > args.endline:
+    exit("Error: Starting line must be lower than ending line.")
 
 print("Loading data from file {}.".format(args.inputfile))
 
 print("Starting from line {}.".format(args.startline))
 
-
+print(args.endline - args.startline)
 
 if args.endline:
     print("Ending at line {}.".format(args.endline))
-    words = readfile(args.inputfile, args.startline, args.endline)
+    train_list, test_list = readfile(
+        args.inputfile, args.test, args.startline, args.endline)
 else:
     print("Ending at last line of file.")
-    words = readfile(args.inputfile, args.startline, args.endline)
+    train_list, test_list = readfile(args.inputfile, args.test, args.startline)
 
-print("Splitting into test and training data.")
-vocab = generate_vocab(words)
-n_grams = list(create_ngram(words, args.ngram))
-one_data = one_hot(vocab, n_grams)
+print("Using {} lines as test data.".format(args.test))
+
+# Train data
+train_vocab = generate_vocab(train_list)
+train_ngrams = list(create_ngram(train_list, args.ngram))
+train_data_encoded = one_hot(train_vocab, train_ngrams)
+
+# Test data
+test_vocab = generate_vocab(test_list)
+test_ngrams = list(create_ngram(test_list, args.ngram))
+test_data_encoded = one_hot(test_vocab, test_ngrams)
 
 print("Constructing {}-gram model.".format(args.ngram))
-print("Writing table to {}.".format(args.outputfile))
-print_to_file(one_data, args.outputfile)
+print("Writing train data table to {}.".format(args.outputfile+".train"))
+print("Writing test data table to {}.".format(args.outputfile+".test"))
+
+print_to_file(train_data_encoded, args.outputfile+".train.csv")  # Train data
+print_to_file(test_data_encoded, args.outputfile+".test.csv")  # Test data
 
 # THERE ARE SOME CORNER CASES YOU HAVE TO DEAL WITH GIVEN THE INPUT
 # PARAMETERS BY ANALYZING THE POSSIBLE ERROR CONDITIONS.
